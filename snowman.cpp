9ced74263de6ba1c8c4b33a7641ac6c831bcbaa9
oriyalperin <73301483+oriyalperin@users.noreply.github.com>
2021-03-10 21:10:51 +0200

    Update snowman.cpp

--- snowman.cpp
+++ snowman.cpp
@@ -13,29 +13,32 @@ array<string,options> larm={larm1,larm2,larm3,larm4};          // returns the le
 array<string,options> rarm={rarm1,rarm2,rarm3,rarm4};           // returns the right arm array<string,options> according to input
 array<string,options> torso= {torso1,torso2,torso3,torso4};    // returns the torso array<string,options> according to input
 array<string,options> base= {base1,base2,base3,base4};   // returns the base string according to input
+const int minDigit=1; //min legal digit
+const int maxDigit=4; //max legal digit
+const int minInput= 11111111;    // minimun input
+const int maxInput= 44444444;    // maxsimum input
+const int inputSize= 8;     // number of digits in input
+const int H= 0;             // first digit in input
+const int N= 1;             // second digit in ipnut
+const int L= 2;             // third digit in input
+const int R= 3;             // fourth digit in input
+const int X= 4;             // Fifth digit in input
+const int Y= 5;             // Sixth count in input
+const int T= 6;             // Seventh digit in the input
+const int B= 7;             // Eighth digit in input
+const int upHand= 1;        //the fourth/Fifth digit in input is 2 = hand up
+const int ones= 10;         //for extract each digit from input
 
 string ariel::snowman(int body)
 {
-    const int min= 11111111;    // minimun input
-    const int max= 44444444;   // maxsimum input
-    const int inputSize= 8;     // number of digits in input
-    const int H= 0;             // first digit in input
-    const int N= 1;             // second digit in ipnut
-    const int L= 2;             // third digit in input
-    const int R= 3;             // fourth digit in input
-    const int X= 4;             // Fifth digit in input
-    const int Y= 5;             // Sixth count in input
-    const int T= 6;             // Seventh digit in the input
-    const int B= 7;             // Eighth digit in input
-    const int upHand= 1;        //the fourth/Fifth digit in input is 2 = hand up
-    const int ones= 10;         //for extract each digit from input
+    
 
-    if(body< min) // if the input: includes 0 or miss digits or is negative
+    if(body< minInput) // if the input: includes 0 or miss digits or is negative
     {
         throw invalid_argument("0 isn't a legal digit / missing digits / negative number");
     }
 
-    if(body> max) // if the input: includes illegal digit or include too much digits
+    if(body> maxInput) // if the input: includes illegal digit or include too much digits
     {
         throw invalid_argument("illegal digit / too much digits"); 
     }  
@@ -46,57 +49,81 @@ string ariel::snowman(int body)
     for(int i=inputSize-1;i>=0;i--) //pass each digit in input
     {
         int digit=temp%ones;
-        if(digit <1 || digit >4)
+        if(digit <minDigit || digit >maxDigit)
         {
             throw invalid_argument("illegal digit");
         }
         sm.at(i)=digit-1; // insert the current digit to the sm array 
         temp/=ones;
     }
+    output(sm);
+    
+    
 
+}
+
+string output(array<int,inputSize> sm)
+{
     if(sm[X]==upHand && sm[Y]!=upHand) //if left hand up and right hand not
     {
         //draw the snowman
-        return
-        hat.at(sm[H])+"\n"+
-        larm.at(sm[X])+
-        leye.at(sm[L])+
-        nose.at(sm[N])+
-        reye.at(sm[R])+"\n "+
-        torso.at(sm[T])+
-        rarm.at(sm[Y])+"\n "+
-        base.at(sm[B]);
+        leftUp(sm);
     }
 
     if (sm[X]!=upHand && sm[Y]==upHand) //if right hand up and left hand not
     {
         //draw the snowman
-        return
-        hat.at(sm[H])+"\n "+
-        leye.at(sm[L])+
-        nose.at(sm[N])+
-        reye.at(sm[R])+
-        rarm.at(sm[Y])+"\n"+
-        larm.at(sm[X])+
-        torso.at(sm[T])+"\n "+
-        base.at(sm[B]);
+        rightUp(sm);
     }
 
     if (sm[X]==upHand && sm[Y]==upHand) //both hands up
     {
         //draw the snowman
-        return
-        hat.at(sm[H])+"\n"+
-        larm.at(sm[X])+
-        leye.at(sm[L])+
-        nose.at(sm[N])+
-        reye.at(sm[R])+
-        rarm.at(sm[Y])+"\n "+
-        torso.at(sm[T])+"\n "+
-        base.at(sm[B]);
+       handsUp(sm);
     }
     
     //if none of the hands are up
+    handsDown(sm);
+}
+string handsUp(array<int,inputSize> sm)
+{
+    return
+    hat.at(sm[H])+"\n"+
+    larm.at(sm[X])+
+    leye.at(sm[L])+
+    nose.at(sm[N])+
+    reye.at(sm[R])+
+    rarm.at(sm[Y])+"\n "+
+    torso.at(sm[T])+"\n "+
+    base.at(sm[B]);
+}
+string rightUp(array<int,inputSize> sm)
+{
+    return
+    hat.at(sm[H])+"\n "+
+    leye.at(sm[L])+
+    nose.at(sm[N])+
+    reye.at(sm[R])+
+    rarm.at(sm[Y])+"\n"+
+    larm.at(sm[X])+
+    torso.at(sm[T])+"\n "+
+    base.at(sm[B]);
+
+}
+string leftUp(array<int,inputSize> sm)
+{ 
+    return
+    hat.at(sm[H])+"\n"+
+    larm.at(sm[X])+
+    leye.at(sm[L])+
+    nose.at(sm[N])+
+    reye.at(sm[R])+"\n "+
+    torso.at(sm[T])+
+    rarm.at(sm[Y])+"\n "+
+    base.at(sm[B]);
+}
+string handsDown(array<int,inputSize> sm)
+{
     return
     hat.at(sm[H])+"\n "+
     leye.at(sm[L])+
@@ -106,6 +133,5 @@ string ariel::snowman(int body)
     torso.at(sm[T])+
     rarm.at(sm[Y])+"\n "+
     base.at(sm[B]);
-
 }
 
